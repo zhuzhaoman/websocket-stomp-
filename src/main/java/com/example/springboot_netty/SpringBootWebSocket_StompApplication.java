e47c1787c61b807fa532400691ca47915164f7b6
zhuzhaoman <2358793988@qq.com>
2020-07-08 20:01:38 +0800

    提交代码

---
+++ src/main/java/com/example/springboot_netty/SpringBootWebSocket_StompApplication.java
@@ -0,0 +1,11 @@
+package com.example.springboot_netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocket_StompApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebSocket_StompApplication.class);
+    }
+}
